170cba56006358c59db186774a534b04fa009f87
yamashita.rintaro@R0141git <yamashita.rintaro@i3design.co.jp>
2023-12-13 16:13:06 +0900

    fix: プロンプトを一旦元に戻す

--- whisper_transcription.py
+++ whisper_transcription.py
@@ -2,161 +2,63 @@ import whisper
 from openai import OpenAI
 import ffmpeg
 import os
-import logging
-import asyncio
+from datetime import datetime
 
-# ロギング設定: エラーとプロセスの進行状況を追跡する
-logging.basicConfig(
-    level=logging.INFO, format="%(asctime)s: %(levelname)s: %(message)s"
+# 入力と出力のパスを設定
+input_video_path = "video/Harvest&Forecast.mp4"
+# output_audio_path = "video/Harvest&Forecast.mp3"
+
+# 入力ファイルの存在をチェック
+if not os.path.exists(input_video_path):
+    raise FileNotFoundError("指定された動画ファイルが見つかりません。")
+
+# ffmpegを使用して音声を抽出
+stream = ffmpeg.input(input_video_path)
+stream = ffmpeg.output(stream, output_audio_path)
+ffmpeg.run(stream)
+
+# Whisperモデルで音声をテキストに変換
+model = whisper.load_model("medium")
+result = model.transcribe("video/Harvest&Forecast.mp4", verbose=True, language="ja")
+
+
+# OpenAI APIキーの存在をチェック
+api_key = os.environ.get("OPENAI_API_KEY")
+if api_key is None:
+    raise EnvironmentError("OPENAI_API_KEYが環境変数に設定されていません。")
+
+client = OpenAI(api_key=api_key)
+
+# 動画を要約
+video_summary = client.chat.completions.create(
+    messages=[
+      {"role": "system", "content": "あなたはSummarizer Proです。Summarizer Proは、提供された動画の文字起こしを確認し、会話のすべての重要な側面をカバーします。主なトピック、決定事項、アクションアイテムの要約に焦点を当てます。全会一致の決定事項や重要な問題を強調します。要約は、簡潔でありながら議論の本質をとらえ、包括的でなければなりません。箇条書きにするか、番号の付いたリストにすると分かりやすい。フォローアップ・アクションのセクションを設け、タスク、期限、責任者を列挙する。会議の目的と出席者を概説する導入部から始める。重要なことを聞き逃さないよう、すべての内容を徹底的に説明する。"},
+      {"role": "user", "content": f"動画の文字起こしを要約し、動画の内容を詳細に解説してください。限界を超えてください。### 動画の文字起こし: {result['text']} ###"},
+    ],
+    model="gpt-4-1106-preview",
 )
+summary_text = video_summary.choices[0].message.content
+print(summary_text)
 
+# 単語リストとビデオ内で出現した時刻を出力
+word_list = client.chat.completions.create(
+    messages=[
+      {"role": "system", "content": "あなたは「ビデオキーワードアナライザー」です。あなたのタスクは、提供されたビデオのトランスクリプトを分析し、重要なキーワードやフレーズを特定し、それらがビデオ内で登場する時刻を記録することです。まず、ビデオのトランスクリプトを徹底的に調査し、頻繁に登場する単語やフレーズ、または議論の主要なポイントに不可欠なものを見つけ出してください。各キーワードやフレーズがビデオのどの時点で初めて登場するかを注意深くメモし、そのタイミングがビデオの再生と一致していることを確認します。特定した各重要な単語やフレーズについて、トピックやコンテキスト内での関連性を簡潔に記述します。最終的な出力は、各キーワード、それに対応する登場時刻、および単語のコンテキストや関連するトピックに関する簡潔な要約を詳細に記載した包括的なリストである必要があります。このアプローチの目的は、ユーザーがビデオの重要なコンポーネントを迅速に理解し、特定の議論やポイントに容易にナビゲートできるようにすることであり、ビデオナラティブ内の各単語の重要性とコンテキストを正確に捉えることに重点を置いています。限界を超えて努力してください。"},
+      {"role": "user", "content": f"以下のタイムスタンプ付きトランスクリプトから重要なキーワードを特定し、各キーワードがビデオに最初に登場した時間と文脈をリストアップしてください。限界を超えてください。### タイムスタンプ付きトランスクリプト: {result['segments']}"},
+    ],
+    model="gpt-4-1106-preview",
+)
+word_list_text = word_list.choices[0].message.content
+print(word_list_text)
 
-class VideoProcessor:
-    """
-    VideoProcessorクラスは、ビデオファイルから音声を抽出し、それをテキストに変換し、
-    さらにそのテキストを要約する処理を行う。
+# 現在の日付と時間を取得
+current_datetime = datetime.now().strftime("%Y%m%d_%H%M%S")
 
-    Attributes:
-        input_video_path (str): 入力ビデオファイルのパス。
-        output_audio_path (str): 出力オーディオファイルのパス。
-        api_key (str): OpenAI APIのキー。
-        client (OpenAI): OpenAI APIのクライアント。
-    """
+# 出力ファイル名に日付と時間を含める
+output_filename = f"transcript/video_summary_{current_datetime}.txt"
 
-    def __init__(self, input_video_path, output_audio_path):
-        """
-        コンストラクタ。入力ビデオパス、出力オーディオパスを受け取り、
-        OpenAI APIのクライアントを初期化する。
-
-        Args:
-            input_video_path (str): 入力ビデオファイルのパス。
-            output_audio_path (str): 出力オーディオファイルのパス。
-        """
-        self.input_video_path = input_video_path
-        self.output_audio_path = output_audio_path
-        self.api_key = os.environ.get("OPENAI_API_KEY")
-        if self.api_key is None:
-            raise EnvironmentError("OPENAI_API_KEYが環境変数に設定されていません。")
-        self.client = OpenAI(api_key=self.api_key)
-
-    def check_file_exists(self, file_path):
-        """
-        指定されたファイルが存在するかチェックする。
-
-        Args:
-            file_path (str): チェックするファイルのパス。
-
-        Raises:
-            FileNotFoundError: 指定されたファイルが存在しない場合に発生。
-        """
-        if not os.path.exists(file_path):
-            raise FileNotFoundError(f"指定されたファイル {file_path} が見つかりません。")
-
-    # ビデオから音声を抽出
-    def extract_audio(self):
-        """
-        ffmpegを使用してビデオファイルから音声を抽出する。
-
-        Raises:
-            Exception: 音声抽出中にエラーが発生した場合に発生。
-        """
-        self.check_file_exists(self.input_video_path)
-        try:
-            stream = ffmpeg.input(self.input_video_path)
-            stream = ffmpeg.output(stream, self.output_audio_path)
-            ffmpeg.run(stream)
-            logging.info("音声の抽出が完了しました。")
-        except Exception as e:
-            logging.error(f"音声抽出中にエラーが発生しました: {e}")
-            raise
-
-    # 音声を文字起こし
-    async def transcribe_audio(self):
-        """
-        Whisperモデルを使用して音声ファイルをテキストに変換する。
-
-        Returns:
-            dict: 変換結果を含む辞書。
-
-        Raises:
-            Exception: テキスト変換中にエラーが発生した場合に発生。
-        """
-        self.check_file_exists(self.output_audio_path)
-        try:
-            model = whisper.load_model("medium")
-            result = model.transcribe(
-                self.output_audio_path, verbose=True, language="ja"
-            )
-            logging.info("音声のテキスト変換が完了しました。")
-            return result
-        except Exception as e:
-            logging.error(f"音声のテキスト変換中にエラーが発生しました: {e}")
-            raise
-
-    # 動画を要約する
-    async def summarize_video(self, text):
-        """
-        OpenAI APIを使用して提供されたテキスト（動画の文字起こし）を要約する。
-
-        Args:
-            text (str): 要約するテキスト。
-
-        Returns:
-            str: 要約されたテキスト。
-
-        Raises:
-            Exception: 要約中にエラーが発生した場合に発生。
-        """
-        try:
-            video_summary = self.client.chat.completions.create(
-                messages=[
-                    {
-                        "role": "system",
-                        "content": "あなたはSummarizer Proです。Summarizer Proは、提供された動画の文字起こしを確認し、会話のすべての重要な側面をカバーします。主なトピック、決定事項、アクションアイテムの要約に焦点を当てます。全会一致の決定事項や重要な問題を強調します。要約は、簡潔でありながら議論の本質をとらえ、包括的でなければなりません。箇条書きにするか、番号の付いたリストにすると分かりやすい。フォローアップ・アクションのセクションを設け、タスク、期限、責任者を列挙する。会議の目的と出席者を概説する導入部から始める。重要なことを聞き逃さないよう、すべての内容を徹底的に説明する。",
-                    },
-                    {
-                        "role": "user",
-                        "content": f"""以下の(#動画の文字起こし)をもとに、(#出力形式)のフォーマットに合わせて動画の内容を詳細に解説してください。限界を超えてください。
-                                      (#出力形式)
-                                      [#会議での重要なトピックと決定事項]
-                                      -
-                                      -
-
-                                      [#フォローアップ・アクション]
-                                      -
-                                      -
-
-                                      (#動画の文字起こし)
-                                      {text}""",
-                    },
-                ],
-                model="gpt-4-1106-preview",
-            )
-            logging.info("動画の要約が完了しました。")
-            return video_summary.choices[0].message.content
-        except Exception as e:
-            logging.error(f"動画の要約中にエラーが発生しました: {e}")
-            raise
-
-
-# メイン関数: ビデオ処理の流れを管理する
-async def main():
-    """
-    メイン関数。ビデオ処理の全体的な流れを管理する。
-
-    以下のステップを実行する:
-    1. VideoProcessorクラスのインスタンスを作成する。
-    2. ビデオから音声を抽出する。
-    3. 音声をテキストに変換する。
-    4. テキストを要約する。
-    """
-    input_video_path = "video/Harvest&Forecast.mp4"
-    output_audio_path = "video/Harvest&Forecast.mp3"
-    processor = VideoProcessor(input_video_path, output_audio_path)
-    processor.extract_audio()
-    transcription_result = await processor.transcribe_audio()
-    summary = await processor.summarize_video(transcription_result["text"])
-    print(summary)
-
-if __name__ == "__main__":
-    asyncio.run(main())
+# 出力をテキストファイルに保存
+with open(output_filename, "w", encoding="utf-8") as file:
+    file.write(summary_text)
+    file.write("\n\n")  # 要約とキーワードリストの間に空行を挿入
+    file.write(word_list_text)
